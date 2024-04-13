1af5f9a10a13949a0d8153f5648b0a9803610ce7
Keitoto <keitotooffice@gmail.com>
2024-04-13 17:30:40 +0200

    ✨ Display target id

--- app/components/Question.tsx
+++ app/components/Question.tsx
@@ -1,5 +1,13 @@
+import { useGameStore } from '@/store/store';
+
 export const Question = () => {
+  const targetId = useGameStore((state) => state.targetId);
+  const initializeGame = useGameStore((state) => state.initializeGame);
+
   return (
-    <div>Question</div>
-  )
-}
+    <div>
+      <button onClick={initializeGame}>start</button>
+      <p>Question {targetId}</p>
+    </div>
+  );
+};
