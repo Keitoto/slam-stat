6f25050575728f11624172991f9a54a3cfd5b57f
Keitoto <keitotooffice@gmail.com>
2024-04-15 17:20:28 +0200

    ✨ Display answers as table

--- app/components/ResultTable.tsx
+++ app/components/ResultTable.tsx
@@ -1,3 +1,62 @@
+import {
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table,
+} from '@/components/ui/table';
+import { useGameStore } from '@/store/store';
+
+const HeadArray = [
+  '',
+  'Name',
+  'Conf',
+  'Team',
+  'MIN',
+  'PTS',
+  'REB',
+  'AST',
+  'FG%',
+  '3P%',
+  'FT%',
+  'STL',
+  'BLK',
+  'TOV',
+];
+
 export const ResultTable = () => {
-  return <div>ResultTable</div>;
+  const answers = useGameStore((state) => state.resultArray);
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          {HeadArray.map((col) => (
+            <TableHead className="w-[100px]">{col}</TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {answers.map((player, i) => (
+          <TableRow>
+            <TableCell className="font-medium">{i + 1}</TableCell>
+            <TableCell className="font-medium">{player.name}</TableCell>
+            <TableCell className="font-medium">{player.conf}</TableCell>
+            <TableCell className="font-medium">{player.team}</TableCell>
+            <TableCell className="font-medium">{player.stats.MIN}</TableCell>
+            <TableCell className="font-medium">{player.stats.PTS}</TableCell>
+            <TableCell className="font-medium">{player.stats.REB}</TableCell>
+            <TableCell className="font-medium">{player.stats.AST}</TableCell>
+            <TableCell className="font-medium">{player.stats.FGP}</TableCell>
+            <TableCell className="font-medium">{player.stats.TPP}</TableCell>
+            <TableCell className="font-medium">{player.stats.FTP}</TableCell>
+            <TableCell className="font-medium">{player.stats.STL}</TableCell>
+            <TableCell className="font-medium">{player.stats.BLK}</TableCell>
+            <TableCell className="font-medium">{player.stats.TOV}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
