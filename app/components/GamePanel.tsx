8c61ffca808caa801d24264d55eb699024a12f70
Keitoto <keitotooffice@gmail.com>
2024-04-15 17:19:24 +0200

    ✨ conditionally  render game UI

--- app/components/GamePanel.tsx
+++ app/components/GamePanel.tsx
@@ -1,13 +1,25 @@
 import { Question } from '@/components/Question';
 import { ResultTable } from '@/components/ResultTable';
 import { SearchBar } from '@/components/SearchBar';
+import StartGame from '@/components/StartGame';
+import { useGameStore } from '@/store/store';
 
 export const GamePanel = () => {
+  const targetId = useGameStore((state) => state.targetId);
+
   return (
-    <div>
-      <Question />
-      <SearchBar />
-      <ResultTable />
+    <div className="flex justify-center flex-col max-w-[80%] m-auto p-8">
+      {!targetId ? (
+        <StartGame />
+      ) : (
+        <>
+          <Question />
+          <div className="h-8"></div>
+          <SearchBar />
+          <div className="h-8"></div>
+          <ResultTable />
+        </>
+      )}
     </div>
   );
 };
