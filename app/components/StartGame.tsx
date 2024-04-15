f55e88e902423e086e0dd06a5d198740281bc187
Keitoto <keitotooffice@gmail.com>
2024-04-15 17:17:19 +0200

    ✨ Add starting panel for the game

---
+++ app/components/StartGame.tsx
@@ -0,0 +1,8 @@
+import { useGameStore } from '@/store/store';
+
+export const StartGame = () => {
+  const startGame = useGameStore((state) => state.startGame);
+  return <button onClick={startGame}>start</button>;
+};
+
+export default StartGame;
