a3d9fd319cdc8450e2d8cc2bbd54a1eaf599c260
Keitoto <keitotooffice@gmail.com>
2024-05-28 22:18:37 +0200

    💄 Styled header and footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer className="h-8 flex items-center justify-center fixed bottom-0 left-0 right-0">
-      Footer
+    <footer className="h-8 flex items-center justify-center text-sm">
+      &copy; 2024 Keitoto Office
     </footer>
   );
 };
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => {
-  return <header className="fixed h-16 border border-b w-full flex justify-between items-center px-4 py-2">
+  return <header className="fixed h-16 border border-b w-full flex justify-between items-center px-4 py-2 bg-white">
     <h1 className="text-xl">Slam Stat</h1>
   </header>;
 };
