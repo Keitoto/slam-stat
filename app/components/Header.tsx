104a1a8fe49f54f2084516424094023bbb767925
Keitoto <keitotooffice@gmail.com>
2024-06-04 13:06:39 +0200

    💄 Fixed z index

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => {
-  return <header className="fixed h-16 border border-b w-full flex justify-between items-center px-4 py-2 bg-white">
+  return <header className="fixed h-16 border border-b w-full flex justify-between items-center px-4 py-2 bg-white z-50">
     <h1 className="text-xl">Slam Stat</h1>
   </header>;
 };
