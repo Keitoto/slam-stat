e9757fa35a713b90818bfc3d95781459d5a353ec
Keitoto <keitotooffice@gmail.com>
2024-06-11 18:52:35 +0200

    ✨ Highlight close answers

--- app/components/HiddenStat.tsx
+++ app/components/HiddenStat.tsx
@@ -7,7 +7,7 @@ export const HiddenStat: FC<{ isOpen: boolean; children: ReactNode }> = ({
   return (
     <div className="relative min-w-20 text-center">
       <div
-        className={`absolute inset-0 bg-gray-300 flex items-center justify-center rounded transition-opacity duration-300 opacity-100 ${isOpen && 'opacity-0'}`}
+        className={`absolute inset-0 bg-gray-300 flex items-center justify-center rounded transition-opacity duration-300 ${isOpen ? 'opacity-0' : 'opacity-100'}`}
       ></div>
       <div className="p-2 bg-white rounded shadow text-md font-bold border border-yellow-200">
         {children}
--- app/components/ResultTable.tsx
+++ app/components/ResultTable.tsx
@@ -25,8 +25,26 @@ const HeadArray = [
   'TOV',
 ];
 
+function isWithinTenPercentRange(value1: number, value2: number): boolean {
+  const lowerBound = value1 * 0.9;
+  const upperBound = value1 * 1.1;
+  return value2 >= lowerBound && value2 <= upperBound;
+}
+
 export const ResultTable = () => {
-  const answers = useGameStore((state) => state.resultArray);
+  const { resultArray, targetPlayer } = useGameStore((state) => state);
+
+  if (!targetPlayer) return;
+
+  const highlightCellClass = (callValue: number, targetValue: number) => {
+    // Correct value
+    if (callValue === targetValue) {
+      return 'green-cell';
+      // Close (10%)
+    } else if (isWithinTenPercentRange(callValue, targetValue)) {
+      return 'yellow-cell';
+    }
+  };
 
   return (
     <Table id="answers">
@@ -40,23 +58,71 @@ export const ResultTable = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {answers.length > 0 &&
-          answers.map((player, i) => (
+        {resultArray.length > 0 &&
+          resultArray.map((player, i) => (
             <TableRow key={player.id}>
-              <TableCell className="font-medium">{i + 1}</TableCell>
-              <TableCell className="font-medium">{player.name}</TableCell>
-              <TableCell className="font-medium">{player.conf}</TableCell>
-              <TableCell className="font-medium">{player.team}</TableCell>
-              <TableCell className="font-medium">{player.stats.MIN}</TableCell>
-              <TableCell className="font-medium">{player.stats.PTS}</TableCell>
-              <TableCell className="font-medium">{player.stats.REB}</TableCell>
-              <TableCell className="font-medium">{player.stats.AST}</TableCell>
-              <TableCell className="font-medium">{player.stats.FGP}</TableCell>
-              <TableCell className="font-medium">{player.stats.TPP}</TableCell>
-              <TableCell className="font-medium">{player.stats.FTP}</TableCell>
-              <TableCell className="font-medium">{player.stats.STL}</TableCell>
-              <TableCell className="font-medium">{player.stats.BLK}</TableCell>
-              <TableCell className="font-medium">{player.stats.TOV}</TableCell>
+              <TableCell className={`font-medium`}>{i + 1}</TableCell>
+              <TableCell className={`font-medium`}>{player.name}</TableCell>
+              <TableCell
+                className={`font-medium ${player.conf === targetPlayer.conf && 'green-cell'}`}
+              >
+                {player.conf}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${player.team === targetPlayer.team && 'green-cell'}`}
+              >
+                {player.team}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.MIN, targetPlayer.stats.MIN)}`}
+              >
+                {player.stats.MIN}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.PTS, targetPlayer.stats.PTS)}`}
+              >
+                {player.stats.PTS}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.REB, targetPlayer.stats.REB)}`}
+              >
+                {player.stats.REB}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.AST, targetPlayer.stats.AST)}`}
+              >
+                {player.stats.AST}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.FGP, targetPlayer.stats.FGP)}`}
+              >
+                {player.stats.FGP}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.TPP, targetPlayer.stats.TPP)}`}
+              >
+                {player.stats.TPP}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.FTP, targetPlayer.stats.FTP)}`}
+              >
+                {player.stats.FTP}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.STL, targetPlayer.stats.STL)}`}
+              >
+                {player.stats.STL}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.BLK, targetPlayer.stats.BLK)}`}
+              >
+                {player.stats.BLK}
+              </TableCell>
+              <TableCell
+                className={`font-medium ${highlightCellClass(player.stats.TOV, targetPlayer.stats.TOV)}`}
+              >
+                {player.stats.TOV}
+              </TableCell>
             </TableRow>
           ))}
       </TableBody>
