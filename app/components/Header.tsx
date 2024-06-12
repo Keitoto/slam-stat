811f0b3afec73d112eca935e5aca1116808b2161
Keitoto <keitotooffice@gmail.com>
2024-06-12 16:12:43 +0200

    ✨ Add logo image

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,10 @@
+import Logo from '@/assets/logo.png';
 export const Header = () => {
-  return <header className="fixed h-16 border border-b w-full flex justify-between items-center px-4 py-2 bg-white z-50">
-    <h1 className="text-xl">Slam Stat</h1>
-  </header>;
+  return (
+    <header className="fixed h-16 border border-b w-full flex justify-between items-center px-4 py-2 bg-white z-50">
+      <h1 className="text-xl w-[120px]">
+        <img src={Logo} alt="Slam-Stat" className="w-full" />
+      </h1>
+    </header>
+  );
 };
