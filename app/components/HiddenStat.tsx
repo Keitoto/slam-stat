345eacfe273a1f25ce91e41f055d6ce8e513c38a
Keitoto <keitotooffice@gmail.com>
2024-06-12 16:14:26 +0200

    ✨ Add custom color for highlight

--- app/components/HiddenStat.tsx
+++ app/components/HiddenStat.tsx
@@ -9,7 +9,7 @@ export const HiddenStat: FC<{ isOpen: boolean; children: ReactNode }> = ({
       <div
         className={`absolute inset-0 bg-gray-300 flex items-center justify-center rounded transition-opacity duration-300 ${isOpen ? 'opacity-0' : 'opacity-100'}`}
       ></div>
-      <div className="p-2 bg-white rounded shadow text-md font-bold border border-yellow-200">
+      <div className="p-2 bg-white rounded shadow text-md font-bold border-2 border-correct">
         {children}
       </div>
     </div>
--- app/components/ShowHintButton.tsx
+++ app/components/ShowHintButton.tsx
@@ -6,10 +6,10 @@ export const ShowHintButton = () => {
   const { revealHint } = useGameStore.getState();
   return (
     <Button
-      className="flex items-center bg-yellow-400 text-black hover:bg-yellow-300"
+      className="flex items-center bg-highlight text-black hover:bg-highlight-hover"
       onClick={revealHint}
     >
-      {<IconLightBulb className="size-4 mr-1" />}Give me some hint
+      {<IconLightBulb className="size-4 mr-1" />}Show Team and Conference
     </Button>
   );
 };
