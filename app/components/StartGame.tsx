4d932976a75690f7bb1c69875d5cbe61e5d2bd61
Keitoto <keitotooffice@gmail.com>
2024-06-12 20:44:59 +0200

    💄 Responsible

--- app/components/StartGame.tsx
+++ app/components/StartGame.tsx
@@ -22,15 +22,15 @@ export const StartGame = () => {
   );
 
   return (
-    <Card className='p-4 border-2 border-black rounded-2xl relative'>
-      <div className='absolute bg-black size-full -z-10 left-3 top-3 rounded-2xl' />
-      <CardHeader>
+    <Card className='py-8 px-4 sm:p-8 border-2 border-black rounded-2xl relative max-w-[500px] w-full mr-1 sm:mr-1.5'>
+      <div className='absolute bg-black size-full -z-10 left-2 sm:left-3 top-2 sm:top-3 rounded-2xl' />
+      <CardHeader className='p-0 mb-6'>
         <CardTitle>Get 7 chances to guess an NBA player.</CardTitle>
         <CardDescription>2023-2024 Regular Season</CardDescription>
       </CardHeader>
-      <CardContent className="children:mb-8">
+      <CardContent className='p-0 mb-6'>
         <div className="flex py-2 items-center">
-          <p className="font-bold w-[160px]">Games Played</p>
+          <p className="font-bold w-[140px] shrink-0">Games Played</p>
           <span className="px-4 text-xl">&ge;</span>
           <Select
             onValueChange={(value: string) => {
@@ -49,7 +49,7 @@ export const StartGame = () => {
           </Select>
         </div>
         <div className="flex py-2 items-center">
-          <p className="font-bold w-[160px]">Minutes per game</p>
+          <p className="font-bold w-[140px] shrink-0">Minutes per game</p>
           <span className="px-4 text-xl">&ge;</span>
           <Select
             onValueChange={(value: string) => {
@@ -69,8 +69,8 @@ export const StartGame = () => {
           </Select>
         </div>
       </CardContent>
-      <CardFooter>
-        <Button onClick={startGame} className='font- text-md'>
+      <CardFooter className='p-0'>
+        <Button onClick={startGame} className='font- text-md max-w-[272px] w-full'>
           Start guessing from {filteredPlayers.length} players
         </Button>
       </CardFooter>
