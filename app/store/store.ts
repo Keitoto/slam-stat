0267e59e2f3bae15c650bd0332efddd990566733
Keitoto <keitotooffice@gmail.com>
2024-06-12 16:13:19 +0200

    ✨ save setting to local storage

--- app/store/store.ts
+++ app/store/store.ts
@@ -4,6 +4,7 @@ import PlayerData from '@/data/data23to24.json';
 import { getPlayerById } from '@/helper/getPlayerById';
 import { getConferenceByTeam } from '@/helper/getConferenceByTeam';
 import { scrollToElementWithSelector } from '@/helper/scrollToElementWithSelector';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 // Default constants
 export const MIN_MINUTE = 10;
@@ -51,97 +52,107 @@ const InitialState = {
   isTeamRevealed: false,
 };
 
-export const useGameStore = create<GameState & GameAction>()((set, get) => ({
-  ...InitialState,
-  startGame: () => {
-    const { filteredPlayers } = get();
-    const randomIndex = Math.floor(Math.random() * filteredPlayers.length) - 1;
-    const targetPlayerId = filteredPlayers[randomIndex].id;
-    const targetPlayer = getPlayerById(targetPlayerId);
-    set(() => ({
-      targetPlayer,
-      isPlaying: true,
-    }));
-  },
-  resetGame: () =>
-    set(() => ({
-      targetPlayer: undefined,
-      remainingLife: NUMBER_OF_LIFE,
-      resultArray: [],
-      isPlaying: false,
-      isSuccess: false,
-      isConfRevealed: false,
-      isTeamRevealed: false,
-    })),
-  checkAnswer: (id: number) => {
-    const { targetPlayer, isConfRevealed, isTeamRevealed } = get();
-    const submittedPlayer = getPlayerById(id);
-
-    if (!targetPlayer || !submittedPlayer) throw new Error();
-
-    if (id === targetPlayer.id) {
-      // CORRECT
-      set((state) => ({
-        isPlaying: false,
-        isSuccess: true,
-        remainingLife: state.remainingLife - 1,
-        isConfRevealed: true,
-        isTeamRevealed: true,
-      }));
-      scrollToElementWithSelector('html', { block: 'start' });
-    } else {
-      // WRONG
-      const remainingLife = get().remainingLife;
-
-      // Game Over
-      if (remainingLife === 1) {
-        set((state) => ({
-          remainingLife: state.remainingLife - 1,
-          resultArray: [...state.resultArray, submittedPlayer],
+export const useGameStore = create<GameState & GameAction>()(
+  persist(
+    (set, get) => ({
+      ...InitialState,
+      startGame: () => {
+        const { filteredPlayers } = get();
+        const randomIndex =
+          Math.floor(Math.random() * filteredPlayers.length) - 1;
+        const targetPlayerId = filteredPlayers[randomIndex].id;
+        const targetPlayer = getPlayerById(targetPlayerId);
+        set(() => ({
+          targetPlayer,
+          isPlaying: true,
+        }));
+      },
+      resetGame: () =>
+        set(() => ({
+          targetPlayer: undefined,
+          remainingLife: NUMBER_OF_LIFE,
+          resultArray: [],
           isPlaying: false,
+          isSuccess: false,
+          isConfRevealed: false,
+          isTeamRevealed: false,
+        })),
+      checkAnswer: (id: number) => {
+        const { targetPlayer, isConfRevealed, isTeamRevealed } = get();
+        const submittedPlayer = getPlayerById(id);
+
+        if (!targetPlayer || !submittedPlayer) throw new Error();
+
+        if (id === targetPlayer.id) {
+          // CORRECT
+          set((state) => ({
+            isPlaying: false,
+            isSuccess: true,
+            remainingLife: state.remainingLife - 1,
+            isConfRevealed: true,
+            isTeamRevealed: true,
+          }));
+          scrollToElementWithSelector('html', { block: 'start' });
+        } else {
+          // WRONG
+          const remainingLife = get().remainingLife;
+
+          // Game Over
+          if (remainingLife === 1) {
+            set((state) => ({
+              remainingLife: state.remainingLife - 1,
+              resultArray: [...state.resultArray, submittedPlayer],
+              isPlaying: false,
+            }));
+
+            scrollToElementWithSelector('html', { block: 'start' });
+
+            // Continue
+          } else {
+            const teamRevealed =
+              isTeamRevealed || targetPlayer.team === submittedPlayer.team;
+
+            const confRevealed =
+              isConfRevealed ||
+              targetPlayer.conf === getConferenceByTeam(submittedPlayer.team);
+
+            set((state) => ({
+              remainingLife: state.remainingLife - 1,
+              resultArray: [...state.resultArray, submittedPlayer],
+              isTeamRevealed: teamRevealed,
+              isConfRevealed: confRevealed,
+            }));
+
+            scrollToElementWithSelector('#answers', { block: 'end' });
+          }
+        }
+      },
+      updateSetting: (min, games) => {
+        const filteredPlayers = (PlayerData as PlayerFullData[]).filter(
+          (player) => player.games > games && player.stats.MIN > min,
+        );
+        set(() => ({
+          setting: { min_games: games, min_minutes: min },
+          filteredPlayers,
         }));
-
-        scrollToElementWithSelector('html', { block: 'start' });
-
-        // Continue
-      } else {
-        const teamRevealed =
-          isTeamRevealed || targetPlayer.team === submittedPlayer.team;
-
-        const confRevealed =
-          isConfRevealed ||
-          targetPlayer.conf === getConferenceByTeam(submittedPlayer.team);
-
-        set((state) => ({
-          remainingLife: state.remainingLife - 1,
-          resultArray: [...state.resultArray, submittedPlayer],
-          isTeamRevealed: teamRevealed,
-          isConfRevealed: confRevealed,
+      },
+      giveUp: () =>
+        set(() => ({
+          isPlaying: false,
+          isConfRevealed: true,
+          isTeamRevealed: true,
+        })),
+      revealHint: () => {
+        set(() => ({
+          isConfRevealed: true,
+          isTeamRevealed: true,
         }));
-
-        scrollToElementWithSelector('#answers', { block: 'end' });
-      }
-    }
-  },
-  updateSetting: (min, games) => {
-    const filteredPlayers = (PlayerData as PlayerFullData[]).filter(
-      (player) => player.games > games && player.stats.MIN > min,
-    );
-    set(() => ({
-      setting: { min_games: games, min_minutes: min },
-      filteredPlayers,
-    }));
-  },
-  giveUp: () =>
-    set(() => ({
-      isPlaying: false,
-      isConfRevealed: true,
-      isTeamRevealed: true,
-    })),
-  revealHint: () => {
-    set(() => ({
-      isConfRevealed: true,
-      isTeamRevealed: true,
-    }));
-  },
-}));
+      },
+    }),
+    {
+      name: 'game-setting', // name of the item in the storage (must be unique)
+      // storage: createJSONStorage(() => sessionStorage), // (optional) by default, 'localStorage' is used
+      partialize: (state) => ({ setting: state.setting }),
+    },
+  ),
+);
